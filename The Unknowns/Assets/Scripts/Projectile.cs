50c1ba297363b0dcded89cd63b83e8d5a0d50134
Matthew R <41610507+MattRDev107@users.noreply.github.com>
2018-08-30 23:01:29 -0500

    fix bug with projectiles

--- The Unknowns/Assets/Scripts/Projectile.cs
+++ The Unknowns/Assets/Scripts/Projectile.cs
@@ -15,11 +15,14 @@ public class Projectile : MonoBehaviour {
     private Rigidbody2D rb2D;
     private PlayerController2D playerController;
     private float randomY;
+    private Vector2 rightDir;
 
     void Start() {
+        rightDir  = transform.right;
         randomY = Random.Range(-spread, spread);
         rb2D = GetComponent<Rigidbody2D>();
         playerController = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController2D>();
+        rightDir = (!playerController.facingRight) ? -rightDir : rightDir;
     }
 
     void Update() {
@@ -34,15 +37,14 @@ public class Projectile : MonoBehaviour {
 
     void FixedUpdate() {
         Vector2 randomSpread = new Vector2(0, randomY);
-        Vector2 rightDir = transform.right;
         Vector2 dir;
-        rightDir = (!playerController.facingRight) ? -rightDir : rightDir;
         dir = rightDir + randomSpread;
         rb2D.velocity = dir.normalized * speed;
     }
 
-    void OnCollisionEnter2D(Collision2D other) {
+    void OnCollisionEnter2D(Collision2D collision) {
+        Instantiate(BlowUp, transform.position, Quaternion.identity);
         Destroy(gameObject);
-        Instantiate(BlowUp,transform.position,Quaternion.identity);
+ 
     }
 }
